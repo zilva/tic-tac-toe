85df725af5c9966340db919c08e6464a26692ce7
PRONTO\csilva <chamitha.silva@pronto.net>
2021-09-20 12:11:48 +0530

    change stylings

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Board from './Board';
-import './Game.css'
 
 const Game = () => {
     return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Component/Game';
+import './index.css';
 
 const root = document.getElementById('root');
 
