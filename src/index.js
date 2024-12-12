5d0dedbbcda880e672752a3cbc81ce7ae7f70806
Chamitha Silva <chamitha.silva@pronto.net>
2024-12-12 21:08:26 +0530

    Updated dependencies and bring back to life

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Game from './Component/Game';
 import './index.css';
 
-const root = document.getElementById('root');
-
-ReactDOM.render(<Game />, root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>
+);
